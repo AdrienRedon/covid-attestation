8292406be59df7e3e37aee4dcd825b8912bfd67a
Adrien REDON <adrien.redon@ibubble.camera>
2020-11-01 10:08:03 +0100

    Attestation based on current location

--- app/automation.js
+++ app/automation.js
@@ -1,15 +1,42 @@
 const puppeteer = require('puppeteer');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 
+/**
+ * 
+ * @return {Object} date_obj
+ * @return {string} date_obj.date
+ * @return {string} date_obj.month
+ * @return {string} date_obj.year
+ * @return {string} date_obj.hours
+ * @return {string} date_obj.minutes
+ */
 const getCurrentDate = () => {
     const date_ob = new Date();
 
     return {
         date: ('0' + date_ob.getDate()).slice(-2),
         month: ('0' + (date_ob.getMonth() + 1)).slice(-2),
-        year: date_ob.getFullYear(),
+        year: '' + date_ob.getFullYear(),
         hours: ('0' + date_ob.getHours()).slice(-2),
-        minutes: date_ob.getMinutes(),
+        minutes: '' + date_ob.getMinutes(),
     };
+}
+
+const getAddress = async (lat, lon) => {
+    try {
+        const response = await axios.get(`https://api-adresse.data.gouv.fr/reverse/?lon=${lon}&lat=${lat}`)
+        return {
+            address: response.data.features[0].properties.name,
+            city: response.data.features[0].properties.city,
+            zipcode: response.data.features[0].properties.postcode,
+        }
+    } catch (e) {
+        console.error(e)
+        return;
+    }
+
 }
 
 /**
@@ -19,22 +46,32 @@ const getCurrentDate = () => {
  * @param {string} requestData.lastname
  * @param {string} requestData.birthday
  * @param {string} requestData.placeofbirth
- * @param {string} requestData.address
- * @param {string} requestData.city
- * @param {string} requestData.zipcode
  */
 const generate = async (requestData) => {
 
+    const dateData = getCurrentDate();
+    const addressData = requestData.lat != null && requestData.lon != null
+       ? await getAddress(requestData.lat, requestData.lon)
+       : null;
+
+    console.log(addressData)
+
     const formData = {
         ...requestData,
-        ...getCurrentDate(),
+        ...dateData,
+        ...addressData,
     };
-    console.log(formData);
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({ headless: true });
+
     const page = await browser.newPage();
-  
+
+    await page._client.send('Page.setDownloadBehavior', {
+        behavior: 'allow',
+        downloadPath: path.join(__dirname, '..', 'public', 'attestations'),
+    });
+
     await page.goto('https://media.interieur.gouv.fr/deplacement-covid-19/');
-  
+
     await page.type('#field-firstname', formData.firstname);
     await page.type('#field-lastname', formData.lastname);
     await page.type('#field-birthday', formData.birthday);
@@ -45,12 +82,23 @@ const generate = async (requestData) => {
     await page.type('#field-datesortie', `${formData.date}/${formData.month}/${formData.year}`);
     await page.type('#field-heuresortie', `${formData.hours}:${formData.minutes}`);
     await page.click('#checkbox-sport_animaux');
-    
+
     await page.click('#generate-btn');
-  
+
     await page.waitForNavigation();
     console.log('New Page URL:', page.url());
     await browser.close();
-  };
+};
 
-module.exports = generate;
+const orderReccentFiles = (dir) =>
+  fs.readdirSync(dir)
+    .filter(f => fs.lstatSync(path.join(dir,f)).isFile())
+    .map(file => ({ file, mtime: fs.lstatSync(path.join(dir, file)).mtime }))
+    .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
+
+const getMostRecentFile = (dir) => {
+  const files = orderReccentFiles(dir);
+  return files.length ? files[0] : undefined;
+};
+
+module.exports = { generate, getMostRecentFile };
--- app/index.js
+++ app/index.js
@@ -1,4 +1,30 @@
-const generate = require('./automation');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const { generate, getMostRecentFile } = require('./automation');
 const requestData = require('./request-data.json');
 
-generate(requestData);
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/generate', async (req, res) => {
+  console.log(req.body);
+  
+  await generate({
+    ...requestData,
+    ...req.body,
+  });
+  res.send(200);
+});
+
+app.use('/last', (req, res) => {
+  const { file } = getMostRecentFile(path.join(__dirname, '..', 'public', 'attestations'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'attestations', file));
+});
+
+app.get('*', express.static(path.join(__dirname, '..', 'public')));
+
+
+app.listen(1337, () => {
+  console.log('server listening on port 1337');
+});
